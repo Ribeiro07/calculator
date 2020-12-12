85018567282ed26572b246988f9598838ee28ff2
Ribeiro <75888776+Ribeiro07@users.noreply.github.com>
2020-12-12 16:51:10 -0300

    Exponent and square root
    Exponent and square root function added

--- calc.cpp
+++ calc.cpp
@@ -3,44 +3,63 @@
 using namespace std;
 
 const string error = "Entrada invalida.";
+const string sqr_type = "sqr";
+const string exp_type = "exp";
 
 int main(){
-    string input;
+    string input, op_type;
     getline(cin, input);
     float x, y, ans;
-    int pos_op;
+    int pos;
 
-    pos_op = input.find('+');
-    if(pos_op!=input.npos){//Its an add function
-        x = stof(input.substr(0, pos_op));
-        y = stof(input.substr(pos_op+1));
+
+    op_type = input.substr(0, 3);
+    if(op_type == sqr_type){//Its an square root function
+        x = stof(input.substr(4));
+        cout << sqrt(x) << endl;
+        return 0;
+    }
+    
+    if(op_type == exp_type){
+        pos = input.find(',');
+        x = stof(input.substr(4, pos));
+        y = stof(input.substr(pos+1));
+        cout << pow(x,y) << endl;
+        return 0;
+    }
+
+
+    pos = input.find('+');
+    if(pos!=input.npos){//Its an add function
+        x = stof(input.substr(0, pos));
+        y = stof(input.substr(pos+1));
         ans = x + y;
         cout << ans << endl;
         return 0;
     }
 
-    pos_op = input.find('*');
-    if(pos_op!=input.npos){//Its an multiply function
-        x = stof(input.substr(0, pos_op));
-        y = stof(input.substr(pos_op+1));
+    pos = input.find('*');
+    if(pos!=input.npos){//Its an multiply function
+        x = stof(input.substr(0, pos));
+        y = stof(input.substr(pos+1));
         ans = x * y;
         cout << ans << endl;
         return 0;
     }
 
-    pos_op = input.find('/');
-    if(pos_op!=input.npos){//Its an divide function
-        x = stof(input.substr(0, pos_op));
-        y = stof(input.substr(pos_op+1));
+    pos = input.find('/');
+    if(pos!=input.npos){//Its an divide function
+        x = stof(input.substr(0, pos));
+        y = stof(input.substr(pos+1));
         ans = x / y;
         cout << ans << endl;
         return 0;
     }
 
-    pos_op = input.find('-',1);
-    if(pos_op!=input.npos){//Its an subtract function
-        x = stof(input.substr(0, pos_op));
-        y = stof(input.substr(pos_op+1));
+    pos = input.find('-',1);
+    if(pos!=input.npos){//Its an subtract function
+        x = stof(input.substr(0, pos));
+        y = stof(input.substr(pos+1));
         ans = x - y;
         cout << ans << endl;
         return 0;
